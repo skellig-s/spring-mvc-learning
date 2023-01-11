c11a5208d0f0c3afc7e9d933554f78664e134f0f
aleksandr <skellig-s@yandex.ru>
2023-01-11 17:01:42 +0100

    init

---
+++ src/main/java/com/apps/ws/ui/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.apps.ws.ui.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+    @GetMapping()
+    public String getUsers(
+        @RequestParam(value="page", defaultValue = "1") int page,
+        @RequestParam(value="limit", defaultValue = "50") int limit
+    ) {
+        return "get user was called with params: " + page + " and " + limit ;
+    }
+
+    @GetMapping(path="/{userId}")
+    public String getUser(@PathVariable String userId) {
+        return "get user was called with user ID = " + userId;
+    }
+
+    @PostMapping
+    public String createUser() {
+        return "create user was called";
+    }
+
+    @PutMapping
+    public String updateUser() {
+        return "update user was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser() {
+        return "delete user was called";
+    }
+}
